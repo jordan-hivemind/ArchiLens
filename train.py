89ba101ea7b9240d6502c5f3360a8396f84e5758
Jordan Schwartz <jordan@hive-mind.com>
2023-09-07 21:07:48 -0700

    Fixing relative path

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ number_of_classes = 25
 
 # Data Preprocessing
 train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=20, width_shift_range=0.2, height_shift_range=0.2, horizontal_flip=True)
-train_generator = train_datagen.flow_from_directory('../architectural-images-training-set/architectural-styles-dataset', target_size=(224, 224), batch_size=32, class_mode='categorical')
+train_generator = train_datagen.flow_from_directory('../architectural-styles-dataset', target_size=(224, 224), batch_size=32, class_mode='categorical')
 
 # Save the class labels to a JSON file
 class_indices = train_generator.class_indices
