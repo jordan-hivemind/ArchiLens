1e06c47e573b435b4b76de903b0e94e8ded45a30
Jordan Schwartz <jordan@hive-mind.com>
2023-09-07 20:48:00 -0700

    Initial commit

---
+++ test.py
@@ -0,0 +1,35 @@
+import json
+import os
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+import numpy as np
+
+# Load the class labels
+with open('class_labels.json', 'r') as f:
+    class_labels = json.load(f)
+
+model = load_model('/Users/jordanschwartz/Library/CloudStorage/Dropbox/z - Miscellaneous/python/ArchiLens/archilens-model.keras')
+
+# Folder containing all test images
+test_folder = "/Users/jordanschwartz/Library/CloudStorage/Dropbox/z - Miscellaneous/python/ArchiLens/test-images"
+
+# Loop through all files in the folder
+for filename in os.listdir(test_folder):
+    if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):  # Add more conditions if you have different file types
+        img_path = os.path.join(test_folder, filename)
+        
+        # Load and preprocess the image; note the change in target_size
+        img = image.load_img(img_path, target_size=(224, 224))
+        img_tensor = image.img_to_array(img)
+        img_tensor = np.expand_dims(img_tensor, axis=0)
+        img_tensor /= 255.
+        
+        # Make the prediction
+        prediction = model.predict(img_tensor)
+        
+        # Interpret the prediction
+        predicted_class = np.argmax(prediction[0])
+        predicted_label = class_labels[str(predicted_class)]  # JSON keys are always strings
+        confidence_score = prediction[0][predicted_class]        
+        
+        print(f'File: {filename}, Predicted class: {predicted_label}, Confidence: {confidence_score:.2f}')
---
+++ train.py
@@ -0,0 +1,35 @@
+import json
+from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+number_of_classes = 25
+
+# Data Preprocessing
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=20, width_shift_range=0.2, height_shift_range=0.2, horizontal_flip=True)
+train_generator = train_datagen.flow_from_directory('/Users/jordanschwartz/Library/CloudStorage/Dropbox/z - Miscellaneous/python/ArchiLens/architectural-images-training-set/architectural-styles-dataset', target_size=(224, 224), batch_size=32, class_mode='categorical')
+
+# Save the class labels to a JSON file
+class_indices = train_generator.class_indices
+class_labels = {v: k for k, v in class_indices.items()}
+
+with open('class_labels.json', 'w') as f:
+    json.dump(class_labels, f)
+
+# Model Architecture
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(number_of_classes, activation='softmax')  # Replace 'number_of_classes' with the actual number of classes
+])
+
+# Compilation
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Training
+model.fit(train_generator, epochs=20)
+
+model.save('/Users/jordanschwartz/Library/CloudStorage/Dropbox/z - Miscellaneous/python/ArchiLens/archilens-model.keras')
