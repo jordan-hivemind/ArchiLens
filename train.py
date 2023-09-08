c5c5bcece25c2fa024e90e5004c345c4b2d64d01
Jordan Schwartz <jordan@hive-mind.com>
2023-09-07 21:19:11 -0700

    fixed relative paths

--- train.py
+++ train.py
@@ -1,12 +1,17 @@
+import os
 import json
 from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 number_of_classes = 25
 
+current_directory = os.path.dirname(os.path.realpath(__file__))
+data_directory = os.path.join(current_directory, 'architectural-images-training-set/architectural-styles-dataset/')
+print(data_directory)
+
 # Data Preprocessing
 train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=20, width_shift_range=0.2, height_shift_range=0.2, horizontal_flip=True)
-train_generator = train_datagen.flow_from_directory('../architectural-styles-dataset', target_size=(224, 224), batch_size=32, class_mode='categorical')
+train_generator = train_datagen.flow_from_directory(data_directory, target_size=(224, 224), batch_size=32, class_mode='categorical')
 
 # Save the class labels to a JSON file
 class_indices = train_generator.class_indices
